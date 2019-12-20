5ea2b1d288d4e5e3974869210a93b8d09acd305f
mrxx99 <mrxx99@googlemail.com>
2019-12-20 22:59:18 +0100

    Add project files.

---
+++ WpfComboBox.NetCore/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfComboBox.NetCore
+{
+    public class MainViewModel
+    {
+        public IEnumerable<double> Values { get; set; } = new[] { 0.2, 0.3, 0.4, 1.0, 2.3, 4.5, 6.7 };
+
+        public double SelectedValue { get; set; }
+    }
+}
---
+++ WpfComboBoxTest.NetFramework/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfComboBoxTest.NetFramework
+{
+    public class MainViewModel
+    {
+        public IEnumerable<double> Values { get; set; } = new[] { 0.2, 0.3, 0.4, 1.0, 2.3, 4.5, 6.7 };
+
+        public double SelectedValue { get; set; }
+
+        public MainViewModel()
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-EN");
+            CultureInfo.CurrentUICulture = new CultureInfo("en-EN");
+        }
+    }
+}
